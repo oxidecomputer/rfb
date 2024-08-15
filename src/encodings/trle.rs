9af7eb1d147f32d54fa673de9dc993c90fe6124e
lif <>
2024-08-15 04:44:23 +0000

    implement subencodings of TRLE

--- src/encodings/trle.rs
+++ src/encodings/trle.rs
@@ -1,3 +1,5 @@
+use std::iter::{from_fn, once};
+
 use crate::encodings::{Encoding, EncodingType};
 use crate::rfb::PixelFormat;
 
@@ -52,14 +54,81 @@ enum TRLETile {
     /// 127
     PackedPaletteReused { packed_pixels: Vec<PackedIndeces> },
     /// 128
-    PlainRLE { color: CPixel, length: usize },
+    PlainRLE { runs: Vec<(CPixel, usize)> },
     /// 129
-    PaletteRLEReused { pixels: Vec<u8> },
+    PaletteRLEReused { runs: Vec<(u8, usize)> },
     /// 130-255
     PaletteRLE {
         palette: Vec<CPixel>,
-        pixels: Vec<u8>,
+        runs: Vec<(u8, usize)>,
     },
+}
+
+fn rle(mut length: usize) -> impl Iterator<Item = u8> {
+    from_fn(move || {
+        if length == 0 {
+            None
+        } else if length > 0xFF {
+            length -= 0xFF;
+            Some(0xFF)
+        } else {
+            let byte = (length - 1) as u8;
+            length = 0;
+            Some(byte)
+        }
+    })
+}
+
+fn pal_rle((index, length): &(u8, usize)) -> Box<dyn Iterator<Item = u8>> {
+    if *length == 1 {
+        Box::new(once(*index))
+    } else {
+        Box::new(once(*index | 0x80).chain(rle(*length)))
+    }
+}
+
+impl TRLETile {
+    /// Subencoding of the tile according to RFB 6143 7.7.5
+    fn encode(&self) -> Box<dyn Iterator<Item = u8> + '_> {
+        match self {
+            TRLETile::Raw { pixels } => {
+                Box::new(once(0u8).chain(pixels.iter().flat_map(|c| c.bytes.iter().copied())))
+            }
+            TRLETile::SolidColor { color } => {
+                Box::new(once(1u8).chain(color.bytes.iter().copied()))
+            }
+            TRLETile::PackedPalette {
+                palette,
+                packed_pixels,
+            } => Box::new(
+                once(palette.len() as u8)
+                    .chain(palette.iter().flat_map(|c| c.bytes.iter().copied()))
+                    .chain(packed_pixels.iter().map(|p| p.0)),
+            ),
+            TRLETile::PackedPaletteReused { packed_pixels } => {
+                Box::new(once(127u8).chain(packed_pixels.iter().map(|p| p.0)))
+            }
+            TRLETile::PlainRLE { runs } => {
+                Box::new(once(128).chain(
+                    runs.iter().flat_map(|(color, length)| {
+                        color.bytes.iter().copied().chain(rle(*length))
+                    }),
+                ))
+            }
+            TRLETile::PaletteRLEReused { runs } => {
+                Box::new(once(129).chain(runs.iter().flat_map(pal_rle)))
+            }
+            TRLETile::PaletteRLE { palette, runs } => Box::new(
+                once(
+                    (palette.len() + 128)
+                        .try_into()
+                        .expect("TRLE tile palette too large!"),
+                )
+                .chain(palette.iter().flat_map(|c| c.bytes.iter().copied()))
+                .chain(runs.iter().flat_map(pal_rle)),
+            ),
+        }
+    }
 }
 
 struct CPixel {
@@ -73,7 +142,10 @@ impl Encoding for TRLEncoding {
     }
 
     fn encode(&self) -> Vec<u8> {
-        todo!()
+        self.tiles
+            .iter()
+            .flat_map(|row| row.iter().flat_map(|tile| tile.encode()))
+            .collect()
     }
 
     fn transform(&self, input: &PixelFormat, output: &PixelFormat) -> Box<dyn Encoding> {
